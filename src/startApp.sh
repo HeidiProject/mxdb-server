e31ff4c77090fb9e6d17c96ed30708ca39ae1927
Kate Smith <kate.smith@psi.ch>
2023-11-14 11:24:01 +0100

    update package dependencies and make template config

--- src/startApp.sh
+++ src/startApp.sh
@@ -2,8 +2,4 @@
 
 export PYTHONPATH=$PYTHONPATH:/opt/mxdb/app:/opt/mxdb/etc:/opt/mxdb/lib
 
-# number of Gunicorn workes 2*NPROC + 1
-NPROC=1
-NWORKERS=`echo "$NPROC*2 + 1" | bc `
-
-gunicorn app:app --worker-class gevent --workers $NWORKERS --certfile /etc/certificates/certs.pem --keyfile /etc/certificates/certs.key --bind 0.0.0.0:5000  --log-file=- --access-logfile=-
+gunicorn app:app --worker-class gevent --workers 1 --certfile /etc/certificates/certs.pem --keyfile /etc/certificates/certs.key --bind 0.0.0.0:5000  --log-file=- --access-logfile=-
