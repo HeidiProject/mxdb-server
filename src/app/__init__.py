8551ec6d9dc76d5da2b085bb4af79d740ab038e8
Kate Smith <katemarylouisesmith@gmail.com>
2024-06-03 13:28:29 +0000

    flatten dewar endpoint

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -17,7 +17,7 @@ appconfig = importlib.import_module("{}".format("appconfig"))
 MONGO_CONNECT_TIMEOUT = timeouts_conf.MONGO_CONNECT_TIMEOUT
 NGINX_UPSTREAM_SERVER_TIMEOUT = timeouts_conf.NGINX_UPSTREAM_SERVER_TIMEOUT
 MONGO_URI = os.getenv("MONGO_URI")
-
+print(f"mongodb://{MONGO_URI}")
 app = Flask(__name__)
 CORS(app)
 #app.config["MONGO_URI"] = appconfig.dbURI
