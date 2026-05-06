c9f1065d00f07af867efa19c6f6822a77b3fed58
May <may.sharpe@psi.ch>
2026-05-06 15:18:23 +0200

    adjusted name of certificates

--- src/startApp.sh
+++ src/startApp.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=$PYTHONPATH:/opt/mxdb/app:/opt/mxdb/etc:/opt/mxdb/lib
 
-gunicorn app:app --worker-class gevent --workers 1 --certfile /etc/certificates/certs.pem --keyfile /etc/certificates/certs.key --bind 0.0.0.0:5000  --log-file=- --access-logfile=-
+gunicorn app:app --worker-class gevent --workers 1 --certfile /etc/certificates/webserver.pem --keyfile /etc/certificates/webserver.key --bind 0.0.0.0:5000  --log-file=- --access-logfile=-
